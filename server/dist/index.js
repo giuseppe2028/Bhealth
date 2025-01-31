7392f7a12164d508a6b7891613bc55cdcb22fce4
Giuseppe2028 <giuseppebarone2028@gmail.com>
2025-01-31 22:53:07 +0100

    fix: update

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,15 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const swagger_json_1 = __importDefault(require("./docs/swagger.json"));
+const routes_1 = __importDefault(require("./routes"));
+dotenv_1.default.config();
+const PORT = process.env.PORT || 3000;
 const app = (0, express_1.default)();
-const PORT = 3000;
-// Middleware per gestire il parsing JSON
 app.use(express_1.default.json());
-// Rotta base
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ error: 'Something went wrong!' });
+});
+(0, routes_1.default)(app);
+//TODO to protect
+app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
 app.get('/', (req, res) => {
     res.send('Welcome to my TypeScript server!');
 });
-// Start del server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
