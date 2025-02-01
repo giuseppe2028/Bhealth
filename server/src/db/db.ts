57a1276c601d5289433bf7618e18c39df72fb1f2
Giuseppe2028 <giuseppebarone2028@gmail.com>
2025-02-01 10:14:56 +0100

    fix: adjustment server

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -10,7 +10,7 @@ const db = new Pool({
     port: Number(process.env.DB_PORT), // Converte la porta in numero , // Database port (default for PostgreSQL)
 });
 // Test connection
-if (process.env.NODE_ENV !== 'test') {
+
     db.connect()
         .then(() => {
             console.log('Connected to the database');
@@ -22,6 +22,6 @@ if (process.env.NODE_ENV !== 'test') {
         .catch(err => {
             console.error('Error during connection to db:', err);
         });
-}
+
 
 export default db;
